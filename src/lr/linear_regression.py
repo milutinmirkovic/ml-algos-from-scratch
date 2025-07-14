4207b65031e81b3954b8d3a712fc65da1a72ef4f
Milutin Mirkovic <milutin.mirkovic@intellya.ai>
2025-07-14 18:21:48 +0200

    linear regression

--- src/lr/linear_regression.py
+++ src/lr/linear_regression.py
@@ -3,57 +3,66 @@ import numpy as np
 
 class LinearRegression:
 
-    def __init__(self,learning_rate:float = 0.1):
-
+    def __init__(self,learning_rate:float = 0.1, max_iterations:int = 1000, tolerance = 1e-6):
+        
         self.learning_rate = learning_rate
+        self.max_iterations = max_iterations
+        self.tolerance = tolerance
         self.W = None
         self.b = None
-        self.y = None
 
-    def initialize_parameters(self,n_features:int):
-
+    def _initialize_parameters(self, n_features:int):
 
         self.W = np.random.randn(n_features)*0.1
-        self.b = 0
+        self.b = 0.0
+
+
+    def compute_loss(self, y_true, predictions):
+
+        m = len(predictions)
+        loss = ((predictions - y_true) ** 2).sum() / m
+
+        return loss
+    
+
+    def compute_gradients(self,X,y_true,predictions):
+
+        residuals = predictions - y_true
+        m = len(residuals)
+
+        dW = np.dot(X.T,residuals) / m
+        db = np.sum(residuals) / m
+
+        return dW, db
 
 
     def forward_pass(self,X):
 
-        predictions = np.dot(X,self.W) + self.b      
+        predictions = np.dot(X,self.W) + self.b
+
         return predictions
+    
+    def backward_pass(self,dW,db):
 
-    def compute_cost(self, predictions):
-
-        m = len(predictions)
-        cost = pow((predictions - self.y),2).sum() / (2*m)
-
-        return cost
+        self.W = self.W - self.learning_rate * dW
+        self.b = self.b - self.learning_rate * db
 
 
-    def backward_pass(self, predictions):
+    def fit(self,X,y):
 
-        m = len(predictions)
-        errors = predictions - self.y
-        self.dW = np.dot(errors,self.X) / m
-        self.db = (predictions - self.y ) / m
+        losses = []
+        for i in range(self.max_iterations):
+            predictions = self.forward_pass(X)
+            loss = self.compute_loss(y,predictions)
+            dW, db = self.compute_gradients(X,y,predictions)
+            self.backward_pass(dW,db)
+            losses.append(loss)
 
+            if i > 0 and abs(losses[-2] - losses[-1]) < self.tolerance:
+                print(f"Converged at iteration {i}")
+                break
 
-    def learn(self, X,y, iterations:int = 100):
-
-        self.X = X
-        self.y = y
-        self.initialize_parameters(X.shape[1])
-        costs = []
-
-        for i in range(iterations):
-            predictions = self.forward_pass(X=X)
-            cost = self.compute_cost(predictions=predictions)
-            self.backward_pass(predictions)
-            self.W -= self.learning_rate * self.dW
-            self.b -= self.learning_rate * self.db
-            costs.append(cost)
-
-        print(costs)
             
+        print(losses)
 
         
--- src/test_bayes.py
+++ src/test_bayes.py
@@ -1,5 +1,7 @@
 import sys
 import os
+from sklearn.model_selection import train_test_split
+
 from sklearn.metrics import classification_report
 
 # Add 'src' to the path
@@ -10,31 +12,32 @@ from naive_bayes.naive_bayes import NaiveBayes
 from core.metrics import confusion_matrix, display_confusion_matrix,classification_metrics
 
 # Full dataset
-df = pd.read_csv(r"C:\ML from scratch\ml-algos-from-scratch\src\Ukradeni.csv")
-X = df.drop(columns="Ukraden")
-y = df["Ukraden"]
+df = pd.read_csv(r"C:\Users\milutin.mirkovic\ml-algos-from-scratch\src\nina_test\drug.csv")
+
+train_df, test_df = train_test_split(df, test_size=0.3, random_state=42)
+X_train = train_df.drop(columns = "Drug")
+X_test = test_df.drop(columns = "Drug")
+y_train = train_df['Drug']
+y_test = test_df['Drug']
 
 
-df_test = pd.read_csv(r"C:\ML from scratch\ml-algos-from-scratch\src\test_set.csv")
-X_test = df_test.drop(columns="Drug")
-y_test = df_test["Drug"]
+
 
 
 model = NaiveBayes(alpha=0.1)
-model.learn(X,y)
+model.learn(X_train,y_train)
 model.show_model()
 
 
 
 
-# predictions = model.predict(X_test)
-# print(predictions)
+predictions = model.predict(X_test)
 
-# conf_matrix = confusion_matrix(y_test,predictions)
-# display_confusion_matrix(conf_matrix)
+conf_matrix = confusion_matrix(y_test,predictions)
+display_confusion_matrix(conf_matrix)
 
-# metrics = classification_metrics(conf_matrix)
-# print(metrics)
+metrics = classification_metrics(conf_matrix)
+print(metrics)
 
 
 
