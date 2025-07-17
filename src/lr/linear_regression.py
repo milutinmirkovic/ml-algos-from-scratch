536ba6f5438f2363263fd22b7469f781fb95bf49
milutinmirkovic <mm20200252@student.fon.bg.ac.rs>
2025-07-18 00:03:13 +0200

    decay, fixed bugs, test for lr

--- src/lr/linear_regression.py
+++ src/lr/linear_regression.py
@@ -1,6 +1,7 @@
 
 import numpy as np
-import math
+import pandas as pd
+
 
 class LinearRegression:
 
@@ -9,8 +10,9 @@ class LinearRegression:
                    max_iterations:int = 1000,
                      tolerance = 1e-6,
                      regularization:str = "none",
-                     regulatization_rate:float = 0.0,
-                     momentum:float =0.0):
+                     regularization_rate:float = 0.0,
+                     momentum:float =0.0,
+                     decay_rate:float= 0.0 ):
         
     
         assert regularization in {"none", "l1", "l2"}, "Invalid regularization type"
@@ -21,10 +23,15 @@ class LinearRegression:
         self.W = None
         self.b = None
         self.regularization = regularization
-        self.alpha = regulatization_rate
+        self.regularization_rate = regularization_rate
         self.momentum = momentum
+        self.decay_rate = decay_rate
         self._momentum_w = None
         self._momentum_b = None
+        self.iteration_step = 0
+        
+        
+    
 
         
 
@@ -35,6 +42,7 @@ class LinearRegression:
 
         self._momentum_b = 0.0
         self._momentum_w = np.zeros(n_features)
+        
 
     def _forward_pass(self,X):
 
@@ -48,9 +56,9 @@ class LinearRegression:
         loss = ((predictions - y_true) ** 2).sum() / m
 
         if self.regularization =="l1":
-            reg = self.alpha * np.abs(self.W).sum()
+            reg = self.regularization_rate * np.abs(self.W).sum()
         elif self.regularization =="l2":
-            reg = self.alpha * np.sum(self.W ** 2)
+            reg = self.regularization_rate * np.sum(self.W ** 2)
         else:
             reg = 0.0
 
@@ -66,34 +74,39 @@ class LinearRegression:
         db = 2 * np.sum(residuals) / m
 
         if self.regularization == "l1":
-            dW += self.alpha * np.sign(self.W) ## izvod od alpha*|W| je +/- alpha
+            dW += self.regularization_rate * np.sign(self.W) ## izvod od alpha*|W| je +/- alpha
 
         elif self.regularization =="l2":
-            dW += 2 * self.alpha * self.W  ## izvod od alpha*W^2 je 2 alpha w
+            dW += 2 * self.regularization_rate * self.W  ## izvod od alpha*W^2 je 2 alpha w
 
         return dW, db
 
     
     def _backward_pass(self,dW,db):
 
+        self.iteration_step += 1
+        lr = self.learning_rate / (1 + self.decay_rate*self.iteration_step)
+
+
         if self.momentum > 0.0:
 
-            self._momentum_w = self.momentum * self._momentum_w + self.learning_rate*dW
-            self._momentum_b = self.momentum * self._momentum_b + self.learning_rate*db
+            self._momentum_w = self.momentum * self._momentum_w + lr*dW
+            self._momentum_b = self.momentum * self._momentum_b + lr*db
 
             self.W = self.W - self._momentum_w
             self.b = self.b - self._momentum_b
 
-            print(self._momentum_b)
-
 
         else:
-            self.W = self.W - self.learning_rate * dW
-            self.b = self.b - self.learning_rate * db
+            self.W = self.W - lr * dW
+            self.b = self.b - lr* db
 
 
 
-    def fit(self,X,y):
+    def fit(self,X:pd.DataFrame, y:pd.Series):
+
+        X = X.values
+        y = y.values
 
         n_features = X.shape[1]
         self._initialize_parameters(n_features=n_features)
@@ -105,12 +118,17 @@ class LinearRegression:
             dW, db = self._compute_gradients(X,y,predictions)
             self._backward_pass(dW,db)
             losses.append(loss)
+        
 
             if i > 0 and abs(losses[-2] - losses[-1]) < self.tolerance:
                 print(f"Converged at iteration {i}")
                 break
+        return losses
+    
 
-    def fit_sgd(self,X,y,bath_size:int = 1, epochs:int = 10):
+
+    def fit_sgd(self,X:pd.DataFrame ,y: pd.Series, batch_size:int = 1, epochs:int = 10):
+
 
         n_rows = X.shape[0]
         n_features = X.shape[1]
@@ -121,34 +139,40 @@ class LinearRegression:
 
             epoch_loss = 0.0
             shuffled_ids = np.random.permutation(n_rows)
-            X_shuffled = X.iloc[shuffled_ids]
-            y_shuffled = y.iloc[shuffled_ids]
+            X_shuffled = X.iloc[shuffled_ids].values
+            y_shuffled = y.iloc[shuffled_ids].values
         
         
-            for start in range(0,n_rows,bath_size):
+            for start in range(0,n_rows,batch_size):
 
                 
-                x_batch = X_shuffled[start:start+bath_size]
-                y_batch = y_shuffled[start:start+bath_size]
+                x_batch = X_shuffled[start:start+batch_size]
+                y_batch = y_shuffled[start:start+batch_size]
 
                 predictions = self._forward_pass(x_batch)
                 loss = self._compute_loss(y_batch,predictions)
                 dW, db = self._compute_gradients(x_batch,y_batch,predictions)
                 self._backward_pass(dW,db)
 
-                epoch_loss+=loss
+                epoch_loss+=loss * len(y_batch)
 
-            avg_loss = epoch_loss / ( math.ceil(n_rows / bath_size))
+            avg_loss = epoch_loss / n_rows
+
             losses.append(avg_loss)
-            #if epoch > 0 and (losses[-2] - losses[-1])<self.tolerance:
-              #  print(f"Converged at epoch {epoch}")
-              #  break
+            
+            if epoch > 0 and (losses[-2] - losses[-1])<self.tolerance:
+                print(f"Converged at epoch {epoch}")
+                break
+            
+        return losses
 
 
             
         
 
-    def predict(self, X_test):
+    def predict(self, X_test:pd.DataFrame):
+        
+        X_test = X_test.values 
         return self._forward_pass(X_test)
 
 
--- src/test_lr.py
+++ src/test_lr.py
@@ -1,46 +1,69 @@
-from lr.linear_regression import LinearRegression 
-from core.metrics import mse, rmse, mae, r_squared
-
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression as SklearnLR
-
-
-
+from lr.linear_regression import LinearRegression
+from core.metrics import mse, rmse, mae, r_squared
 
 data = pd.read_csv(r"C:\ML from scratch\ml-algos-from-scratch\src\lr\boston.csv")
 X = data.drop("MEDV", axis=1)
 y = data["MEDV"]
 
-
-
-X = (X - X.mean()) / X.std()
-
-# Train/test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-
-X_mean = X_train.mean(axis=0)
-X_std = X_train.std(axis=0)
-
+X_mean = X_train.mean()
+X_std = X_train.std()
 X_train = (X_train - X_mean) / X_std
 X_test = (X_test - X_mean) / X_std
 
+models = {
+    "BGD (no reg)": LinearRegression(learning_rate=0.1, max_iterations=100,decay_rate = 0.01),
+    "BGD":LinearRegression(learning_rate=0.01, max_iterations=100,decay_rate = 0.01,momentum=0.2),
+    "BGD momentum":LinearRegression(learning_rate=0.01, max_iterations=100,decay_rate = 0.01),
+    "BGD l1":LinearRegression(learning_rate=0.01, max_iterations=100,decay_rate = 0.01,regularization="l1",regularization_rate=0.1),
+    "BGD l2":LinearRegression(learning_rate=0.01, max_iterations=100,decay_rate = 0.01,regularization="l2",regularization_rate=0.1),
+    "BGD l2":LinearRegression(learning_rate=0.01, max_iterations=100,decay_rate = 0.01,regularization="l2",regularization_rate=0.1),
 
 
-model = LinearRegression(
-    learning_rate=0.1,
-    max_iterations=1000,
-    regularization="l1",
-    regulatization_rate=0.3,
-    momentum=0.1
-)
+}
 
-model.fit(X_train,y_train)
+losses = {}
+results = {}
 
-preds = model.predict(X_test)
+for name, model in models.items():
+    if "SGD" in name:
+        loss = model.fit_sgd(X_train, y_train, batch_size=16, epochs=100)
+    else:
+        loss = model.fit_sgd(X_train, y_train,batch_size=2,epochs=4)
+    y_pred = model.predict(X_test)
+    losses[name] = loss
+    results[name] = {
+        "MAE": mae(y_test, y_pred),
+        "RMSE": rmse(y_test, y_pred),
+        "R2": r_squared(y_test, y_pred)
+    }
 
-print(f"RMSE: {rmse(y_test,preds)}")
-print(f"R squared: {r_squared(y_test,preds)}")
+sk_model = SklearnLR()
+sk_model.fit(X_train, y_train)
+sk_preds = sk_model.predict(X_test)
+results["Sklearn"] = {
+    "MAE": mae(y_test, sk_preds),
+    "RMSE": rmse(y_test, sk_preds),
+    "R2": r_squared(y_test, sk_preds)
+}
 
+for name, metrics in results.items():
+    print(f"{name}")
+    for metric_name, value in metrics.items():
+        print(f"{metric_name}: {value:.4f}")
+    print()
 
+plt.figure(figsize=(10, 5))
+for name, l in losses.items():
+    plt.plot(l, label=name)
+plt.xlabel("Epoch / Iteration")
+plt.ylabel("Loss")
+plt.title("Loss Curves")
+plt.legend()
+plt.tight_layout()
+plt.show()
